ea9ff5d56a4c53798de8b0b648b3ec11d9abbf34
Md. Ashadousjaman Rony <ashadousjaman.rony@bjitacademy.com>
2023-06-19 11:12:05 +0600

    Polymorphism example for Shape.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle();
+        Shape circle = new Circle();
+        Shape triangle = new Triangle();
+
+        rectangle.draw();
+        circle.draw();
+        triangle.draw();
+
+    }
+}
